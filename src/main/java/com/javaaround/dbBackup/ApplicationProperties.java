9cd6c3c0ac6b21b0ce8c5663258a0f0fc1a52d4c
Md.shamim Miah <shamim.ict0754@gmail.com>
2019-09-06 09:03:40 +0600

    send commit

--- src/main/java/com/javaaround/dbBackup/ApplicationProperties.java
+++ src/main/java/com/javaaround/dbBackup/ApplicationProperties.java
@@ -4,13 +4,15 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "backup.database")
 public class ApplicationProperties {
 
     private String databaseIp;
-    private String databaseName;
+    private List<String> databases;
     private String databaseUserName;
     private String databaseUserPassword;
     private BackupClientInfo backupClientInfo;
--- src/main/java/com/javaaround/dbBackup/ScheduledTasks.java
+++ src/main/java/com/javaaround/dbBackup/ScheduledTasks.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class ScheduledTasks {
@@ -19,42 +22,42 @@ public class ScheduledTasks {
    // @Scheduled(cron = "${cron.expression}")
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
-        try {
-            /*
-            boolean isWindows = System.getProperty("os.name")
-                    .toLowerCase().startsWith("windows");
-            System.out.println(isWindows);*/
+        applicationProperties.getDatabases().forEach(databaseName -> {
+            try {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd h:m a");
+                String todaysDateString = LocalDateTime.now().format(formatter).toString();
 
-           File backupFile = new File("E:/java/practice/backup/blog_" + LocalDateTime.now().toString()
-                   .replace("T", "")
-                   .replaceAll(":", "-")
-                   .split("\\.")[0]
-                   + ".sql");
+                File backupFile = new File(applicationProperties.getBackupClientInfo().getBackupLocation() + "/" + databaseName + "_" +
+                        todaysDateString
+                                .replace(" ", "_")
+                                .replace(":", "_")
+                        + ".sql");
 
-            /*File file = new File("e://java//practice//backup//blog_"+now.format(dateTimeFormatter)+".sql");*/
+                /*File file = new File("e://java//practice//backup//blog_"+now.format(dateTimeFormatter)+".sql");*/
 
-           //Create the file
-            if (backupFile.createNewFile())
-            {
-                System.out.println("File is created!");
-            } else {
-                System.out.println("File already exists.");
+                //Create the file
+                if (backupFile.createNewFile()) {
+                    System.out.println("File is created!");
+                } else {
+                    System.out.println("File already exists.");
+                }
+                //if(backupFile.createNewFile()){
+                Process runtimeProcess = Runtime.getRuntime().exec(applicationProperties.getBackupClientInfo().getToolLocation() + "/mysqldump -u" + applicationProperties.getDatabaseUserName() + " -h" + applicationProperties.getDatabaseIp() + " -p" + applicationProperties.getDatabaseUserPassword() + " -B " + databaseName + " -r " + backupFile.getAbsolutePath());
+                int processComplete = runtimeProcess.waitFor();
+
+                /*NOTE: processComplete=0 if correctly executed, will contain other values if not*/
+                if (processComplete == 0) {
+                    System.out.println("Backup Complete");
+                } else {
+                    System.out.println("Backup Failure");
+                }
+                //   }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            //if(backupFile.createNewFile()){
-            Process runtimeProcess = Runtime.getRuntime().exec(applicationProperties.getBackupClientInfo().getToolLocation() + "/mysqldump -u" + applicationProperties.getDatabaseUserName() + " -h" + applicationProperties.getDatabaseIp() + " -p" + applicationProperties.getDatabaseUserPassword() + " -B " + applicationProperties.getDatabaseName() + " -r " + applicationProperties.getBackupClientInfo().getBackupLocation());
-            int processComplete = runtimeProcess.waitFor();
+        });
 
-            /*NOTE: processComplete=0 if correctly executed, will contain other values if not*/
-            if (processComplete == 0) {
-                System.out.println("Backup Complete");
-            } else {
-                System.out.println("Backup Failure");
-            }
-         //   }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
